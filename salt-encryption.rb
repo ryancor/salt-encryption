e5b19f0c08bfa12a0f890b5bd83517beb51510a8
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2017-05-22 15:07:11 -0700

    store encrypted keys

--- salt-encryption.rb
+++ salt-encryption.rb
@@ -1,6 +1,11 @@
 require 'zlib'
 
 class Ringo
+	def self.store_key(key)
+		file = File.open("test.txt", 'w')
+		file.write(key)
+	end
+
 	def self.encrypt(str)
    s = str.split("")
    arr = []
@@ -14,40 +19,46 @@ class Ringo
 		  end
     end
     encrypted_string = Zlib::Deflate.deflate(arr.join(""))
+		file = File.open("test.txt", 'w')
+		file.write(encrypted_string)
 		return encrypted_string
 	end
 
-	def self.decrypt(str)
+	def self.decrypt
 	 begin
-   	 s = Zlib::Inflate.inflate(str)
-		 split = s.split("")
-	   arr = []
-	   split.each_with_index do |v, i|
-			  if i.between?(0, 5)
-		       arr << (v.codepoints.first - 3).chr
-			  elsif i.between?(6, 10)
-		  	   arr << (v.codepoints.first - 8).chr
-		    elsif i > 10
-			     arr << (v.codepoints.first - 5).chr
-			  end
-	    end
-	    decrypted_string = arr.join("")
-			return decrypted_string
+		 File.open("test.txt").each do |line|
+			 s = Zlib::Inflate.inflate(line)
+			 split = s.split("")
+		   arr = []
+		   split.each_with_index do |v, i|
+				  if i.between?(0, 5)
+			       arr << (v.codepoints.first - 3).chr
+				  elsif i.between?(6, 10)
+			  	   arr << (v.codepoints.first - 8).chr
+			    elsif i > 10
+				     arr << (v.codepoints.first - 5).chr
+				  end
+		    end
+		    decrypted_string = arr.join("")
+				return decrypted_string
+		 end
 		rescue Exception => e
  		 puts "Issue: #{e}"
  	 end
 	end
 end
 
-puts "Encrypt or Decrypt?"
+puts "Encrypt, Decrypt, or Store Key?"
 answer = gets.chomp
 
 if ['Encrypt', 'encrypt'].include?(answer)
 	puts "Enter text:"
 	msg = gets.chomp
 	p "\nEncrypted Message: #{Ringo.encrypt(msg)}"
-else
-	puts "Enter encrypted text:"
+elsif ['Store', 'store'].include?(answer)
+	puts "Enter key:"
 	msg = gets.chomp
-	p "\nDecrypted Message: #{Ringo.decrypt(msg)}"
+	Ringo.store_key(msg)
+else
+	p "\nDecrypted Message: #{Ringo.decrypt}"
 end
