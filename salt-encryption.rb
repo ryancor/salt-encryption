f7f63f31da0a1d823cbfa103190d9c1de423d697
brett bergin <brett.bergin@ring.com>
2017-05-23 22:29:56 -0700

    #2 Remove Compression Logic | Move keystone to local user path.

--- salt-encryption.rb
+++ salt-encryption.rb
@@ -2,50 +2,40 @@ require 'zlib'
 
 class Ringo
 	def self.store_key(key)
-		file = File.open("store_keys.txt", 'w')
+		file = File.open("~/.store_keys.txt", 'w')
 		file.write(key)
 	end
 
 	def self.encrypt(str)
-   s = str.split("")
-   arr = []
-   s.each_with_index do |v, i|
-		  if i.between?(0,5)
-	       arr << (v.codepoints.first + 3).chr
-		  elsif i.between?(6, 10)
-	  	   arr << (v.codepoints.first + 8).chr
-	    elsif i > 10
-		     arr << (v.codepoints.first + 5).chr
-		  end
-    end
-    encrypted_string = Zlib::Deflate.deflate(arr.join(""))
-		file = File.open("store_keys.txt", 'w')
-		file.write(encrypted_string)
-		return encrypted_string
+		s = str.split("")
+		arr = []
+		s.each_with_index do |v, i|
+			if i.between?(0,5)
+				arr << (v.codepoints.first + 3).chr
+		 	elsif i.between?(6, 10)
+				arr << (v.codepoints.first + 8).chr
+			elsif i > 10
+				arr << (v.codepoints.first + 5).chr
+			end
+		end
+		return arr.join("")
 	end
 
-	def self.decrypt
-	 begin
-		 File.open("store_keys.txt").each do |line|
-			 s = Zlib::Inflate.inflate(line)
-			 split = s.split("")
-		   arr = []
-		   split.each_with_index do |v, i|
-				  if i.between?(0, 5)
-			       arr << (v.codepoints.first - 3).chr
-				  elsif i.between?(6, 10)
-			  	   arr << (v.codepoints.first - 8).chr
-			    elsif i > 10
-				     arr << (v.codepoints.first - 5).chr
-				  end
-		    end
-		    decrypted_string = arr.join("")
-				return decrypted_string
-		 end
-		rescue Exception => e
- 		 puts "Issue: #{e}"
- 	 end
+	def self.decrypt(str)
+		s = str.split("")
+		arr = []
+		split.each_with_index do |v, i|
+			if i.between?(0, 5)
+				arr << (v.codepoints.first - 3).chr
+			elsif i.between?(6, 10)
+				arr << (v.codepoints.first - 8).chr
+			elsif i > 10
+				arr << (v.codepoints.first - 5).chr
+			end
+		end
+		return arr.join("")
 	end
+
 end
 
 puts "Encrypt, Decrypt, or Store Key?"
